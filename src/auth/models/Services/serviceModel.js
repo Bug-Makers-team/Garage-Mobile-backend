f48947dae382eb76bb1e1103d0f72dfaa91b123e
Aseel Alasaad <aseelalasaad@gmail.com>
2021-11-25 14:26:08 +0200

    signup singin

---
+++ src/auth/models/Services/serviceModel.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const serviceModel = (sequelize, DataTypes) => sequelize.define('Service', {
+  name: { type: DataTypes.STRING, required: true },
+  description: { type: DataTypes.STRING, required: true },
+  price: { 
+      type: DataTypes.INTEGER,
+       required: true }
+});
+
+module.exports = serviceModel;
