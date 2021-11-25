6f305bc64903f4bdb9c82ec58962a944c8f98367
DimaAlabsi <dema.yaser8@gmail.com>
2021-11-25 11:39:49 +0200

    starter code

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const { start } = require('./src/server');
+const { db } = require('./src/auth/models');
+const PORT =process.env.PORT;
+// we first connect to the DB, then we run our server
+db.sync().then(() => {
+  // kickstart the server
+  start(PORT); // will start our server
+}).catch(console.error);
