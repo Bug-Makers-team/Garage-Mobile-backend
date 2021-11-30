a07424ae8af7352f35f508a3afa45725cde0b2af
RamiZregat <rami.zregat97@gmail.com>
2021-11-30 13:43:47 +0200

    done

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -7,7 +7,7 @@ const Collection=require('./date-Collection.js');
 
 const { Sequelize, DataTypes } = require('sequelize');
 
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
 
 
 let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
