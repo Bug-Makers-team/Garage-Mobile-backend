b7a783b1716d85535b141d1f4674c1b5e282a573
RamiZregat <rami.zregat97@gmail.com>
2022-01-05 16:49:37 +0200

    done

--- src/auth/models/myservices.js
+++ src/auth/models/myservices.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const myServiceModel = (sequelize, DataTypes) => sequelize.define('MyServicess', {
+const myServiceModel = (sequelize, DataTypes) => sequelize.define('MyService', {
   user_id: { type: DataTypes.STRING, required: true },
-  nameOfServices: { type: DataTypes.STRING, required: true,unique: true },
+  nameOfServices: { type: DataTypes.STRING, required: true},
   description: { type: DataTypes.STRING, required: true },
   price: { 
       type: DataTypes.STRING,
